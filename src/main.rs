33549caa8c1505dc7db67d935634a84a02c5601f
Abel Goy <73681996+agoy184@users.noreply.github.com>
2026-02-10 19:08:23 -0800

    working with warning
    Warnings for some lines in animate_player, one line in the master repo doesnt exist for chapter 1 (line 128 for mine doesnt exist in here-> https://github.com/jamesfebin/ImpatientProgrammerBevyRust/blob/main/chapter1/src/player.rs#L68 )

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,22 @@
 use bevy::prelude::*;
+use crate::player::PlayerPlugin;
 
-#[derive(Component)]
-struct Player;
+mod player;
 
 fn main() {
     App::new()
-    .add_plugins(DefaultPlugins)
-    .add_systems(Startup, setup)
-    .add_systems(Update, move_player)
-    .run();
+        .insert_resource(ClearColor(Color::WHITE))
+        .add_plugins(
+            DefaultPlugins.set(AssetPlugin {
+                file_path: "src/assets".into(),
+                ..default()
+            }),
+        )
+        .add_systems(Startup, setup_camera)
+        .add_plugins(PlayerPlugin) // Update this line
+        .run();
 }
 
-fn setup(mut commands: Commands) {
+fn setup_camera(mut commands: Commands) {
     commands.spawn(Camera2d);
-
-    commands.spawn((
-        Text2d::new("@"),
-        TextFont {
-            font_size: 12.0,	
-            font: default(),
-            ..default()
-        },
-        TextColor(Color::WHITE),
-        Transform::from_translation(Vec3::ZERO),
-        Player,
-    ));
 }
-
-fn move_player(
-    // "Bevy, give me keyboard input"
-    input: Res<ButtonInput<KeyCode>>,           
-    // "Bevy, give me the game timer"
-    time: Res<Time>,                            
-    // "Bevy, give me the player's position"
-    mut player_transform: Single<&mut Transform, With<Player>>, 
-) {
-    let mut direction = Vec2::ZERO;
-    if input.pressed(KeyCode::ArrowLeft) {
-        direction.x -= 1.0;
-    }
-    if input.pressed(KeyCode::ArrowRight) {
-        direction.x += 1.0;
-    }
-    if input.pressed(KeyCode::ArrowUp) {
-        direction.y += 1.0;
-    }
-    if input.pressed(KeyCode::ArrowDown) {
-        direction.y -= 1.0;
-    }
-
-    if direction != Vec2::ZERO {
-        let speed = 300.0; // pixels per second
-        let delta = direction.normalize() * speed * time.delta_secs();
-        player_transform.translation.x += delta.x;
-        player_transform.translation.y += delta.y;
-    }
-}
-
-
-
